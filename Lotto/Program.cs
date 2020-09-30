ee592125eefbea7f428f38980d2d74b42ac4cc48
KlaasHagge <klaash@ppedv.de>
2020-09-30 10:53:59 +0200

    Eumeratoren, Funktionen  + Übung (kommentiert)

--- Lotto/Program.cs
+++ Lotto/Program.cs
@@ -13,40 +13,45 @@ namespace Lotto
             int[] zufallsZahlen = new int[3];
             int treffer = 0;
 
-            //Schleife zum Würfeln der Zahlen
-            for (int i = 0; i < zufallsZahlen.Length; i++)
+            do
             {
-                //Deklaration eines Zwischenspeichers
-                int temp;
-                do
+                //Schleife zum Würfeln der Zahlen
+                for (int i = 0; i < zufallsZahlen.Length; i++)
                 {
-                    //Würfeln der Zahl in den Zwischenspeicher
-                    temp = generator.Next(1, 11);
-                    //Wdh der Schleife, wenn Zahl bereits im Array
-                } while (zufallsZahlen.Contains(temp));
-                //Übertrag des Zwischenspeichers in das Array
-                zufallsZahlen[i] = temp;
-            }
+                    //Deklaration eines Zwischenspeichers
+                    int temp;
+                    do
+                    {
+                        //Würfeln der Zahl in den Zwischenspeicher
+                        temp = generator.Next(1, 11);
+                        //Wdh der Schleife, wenn Zahl bereits im Array
+                    } while (zufallsZahlen.Contains(temp));
+                    //Übertrag des Zwischenspeichers in das Array
+                    zufallsZahlen[i] = temp;
+                }
 
-            //Schleife zum Erfragen der Benutzerzahlen
-            for (int i = 0; i < benutzerZahlen.Length; i++)
-            {
-                Console.Write("Gib eine Zahl zwischen 1 und 10 ein: ");
-                benutzerZahlen[i] = int.Parse(Console.ReadLine());
-            }
+                //Schleife zum Erfragen der Benutzerzahlen
+                for (int i = 0; i < benutzerZahlen.Length; i++)
+                {
+                    Console.Write("Gib eine Zahl zwischen 1 und 10 ein: ");
+                    benutzerZahlen[i] = int.Parse(Console.ReadLine());
+                }
 
-            Console.Write("\nGewinnzahlen:");
-            //Schleife zum Ausgeben der Gewinnzahlen UND zum Zählen der Treffer
-            foreach (var zahl in zufallsZahlen)
-            {
-                Console.Write($" {zahl}");
-                //Hochzählen der Treffervariablen bei Vorkommen in beiden Arrays
-                if (benutzerZahlen.Contains(zahl))
-                    treffer++;
-            }
+                Console.Write("\nGewinnzahlen:");
+                //Schleife zum Ausgeben der Gewinnzahlen UND zum Zählen der Treffer
+                foreach (var zahl in zufallsZahlen)
+                {
+                    Console.Write($" {zahl}");
+                    //Hochzählen der Treffervariablen bei Vorkommen in beiden Arrays
+                    if (benutzerZahlen.Contains(zahl))
+                        treffer++;
+                }
 
-            //Ausgabe der Treffer:
-            Console.WriteLine($"\n\nDu hast {treffer} richtige Zahl(en) erraten.");
+                //Ausgabe der Treffer:
+                Console.WriteLine($"\n\nDu hast {treffer} richtige Zahl(en) erraten.");
+
+                Console.WriteLine("Nochmal? Drücke Y!");
+            } while (Console.ReadKey(true).Key == ConsoleKey.Y);
          }
     }
 }
